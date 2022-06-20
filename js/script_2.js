a7de08340f28b5c443afe2fca5773115692a13ac
joceilton <joceilton.g@gmail.com>
2022-06-20 11:08:16 -0300

    Create script_2.js

---
+++ js/script_2.js
@@ -0,0 +1,44 @@
+ $(document).ready(function() { 
+
+    var acertos = 0;
+    var total = 0;
+
+    function atualiza(timer) {
+
+        setInterval(function() {
+
+            const m_top = Math.floor((Math.random() * $('.areaGamer').height()) + 1);
+            const m_left = Math.floor((Math.random() * $('.areaGamer').width()) + 1); 
+
+            $(".bloco").css({ 'margin-top': m_top+ 'px', 'margin-left':m_left+ 'px' });
+
+            $(".acertos").text(acertos)
+
+            $(".total").text(total += 1)
+
+        }, timer)
+        
+    }
+
+    setTimeout(function() {
+        $(".mascara").addClass(".ocultar")
+    }, 3000)
+            
+    atualiza(1000)
+
+    $(".bloco.habilitar").on("click", function() {
+        var bloco = $(this)
+        bloco.css({"border": "1px #f00 solid"})
+        bloco.find("img").attr("src", "img/mancha.png")
+        bloco.removeClass("habilitar")
+        setTimeout(function() {
+            bloco.css({"border": "0"})
+            bloco.find("img").attr("src", "img/inseto.png")
+            bloco.addClass("habilitar")
+            acertos += 1
+        }, 15)
+    })
+    
+
+
+});
